86357c18fec324c0b02fc5c3af757721925b2002
jinexus1 <ap2929470@gmail.com>
2024-07-13 22:56:36 +0530

    Day 1: Variable and Data Types

---
+++ Day1.js
@@ -0,0 +1,34 @@
+//Activity 1: Variable Declaration
+
+//Task 1
+var num=10
+console.log(num);
+//Task 2
+let name="Jinexus"
+console.log(name);
+//Activity 2: Constant Declaration
+
+//Task 3
+const bool=true
+console.log(bool);
+//Activity 3: Data Type
+//Task 4
+let arr=[]
+let obj={}
+console.log(typeof(num));
+console.log(typeof (arr));
+console.log(typeof (name));
+console.log(typeof bool);
+
+//Activity 4: Reassinging variables
+//Task 5
+let a=5
+console.log(a);
+a=6
+console.log(a);
+
+//Task 6
+const b=0
+console.log(b);
+b=1
+console.log(b);//TypeError: Assignment to constant variable.
