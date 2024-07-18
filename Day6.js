73be72632a2a4778b80019655c403742941a5ab7
jinexus1 <ap2929470@gmail.com>
2024-07-18 09:49:39 +0530

    Day 6

--- Day6.js
+++ Day6.js
@@ -13,3 +13,31 @@ console.log(arr);
 
 arr.unshift(1);
 console.log(arr);
+
+let doubleArr=arr.map(arr=>arr*2);
+console.log(doubleArr);
+
+let evenArr=arr.filter(arr=>arr%2==0)
+console.log(evenArr);
+
+let reduceArr=arr.reduce(arr=>arr+=arr)
+console.log(reduceArr);
+
+for (let index = 0; index < arr.length; index++) {
+  console.log( arr[index]);
+  
+}
+arr.forEach(element => {
+  console.log(element);
+})
+
+let matrix=[[1,2,3],[2,3,4],[5,6,7]]
+for (let i = 0; i < matrix.length; i++) {
+  let row = "";
+  for (let j = 0; j < matrix[i].length; j++) {
+    row += matrix[i][j] + " ";
+  }
+  console.log(row.trim());
+}
+
+
