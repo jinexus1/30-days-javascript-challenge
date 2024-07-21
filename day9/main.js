5c1e205bab2ca983019c5d24518b5ef1bc3f2bbb
jinexus1 <ap2929470@gmail.com>
2024-07-21 11:55:19 +0530

    day-9

--- day9/main.js
+++ day9/main.js
@@ -1,21 +1,25 @@
-//Activity-1
+// Activity-1
 document.getElementById("demo").innerHTML = "Hello World!";
-document.getElementsByClassName("dem02").innerHTML = " hello this is demo to";
-//Activity-2
+document.getElementsByClassName("dem02")[0].innerHTML =
+  " hello this is demo to";
+
+// Activity-2
 let newDiv = document.createElement("div");
-
-// Add content to the div
 newDiv.innerHTML = "Hello, this is a new div!";
-
-// Append the div to the body
 document.body.appendChild(newDiv);
+
 let newLi = document.createElement("li");
 newLi.innerHTML = "Item 3";
 document.getElementById("un-list").appendChild(newLi);
-let removeElement = document.getElementById("remove");
 
+let removeElement = document.getElementById("remove");
 if (removeElement) {
-  //if the removeElement exit
   removeElement.remove();
 }
 
+function removeLastListElement() {
+  let ul = document.getElementById("un-list");
+  if (ul.children.length > 0) {
+    ul.removeChild(ul.lastElementChild);
+  }
+}
