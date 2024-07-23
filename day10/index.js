6acfa51069ae13605de1b027e4b446ab3a80f72b
jinexus1 <ap2929470@gmail.com>
2024-07-23 20:29:44 +0530

    10

---
+++ day10/index.js
@@ -0,0 +1,26 @@
+// script.js
+const button = document.getElementById("myButton");
+const paragraph = document.getElementById("myParagraph");
+const image = document.getElementById("myImage");
+
+button.addEventListener("click", function () {
+  paragraph.textContent = "This is the new text!";
+});
+
+image.addEventListener("dblclick", function () {
+  image.style.display = image.style.display === "block" ? "none" : "block";
+});
+const hoverElement = document.getElementById('hoverElement');
+
+        hoverElement.addEventListener('mouseover', function() {
+            hoverElement.style.backgroundColor = 'lightcoral';
+        });
+
+        hoverElement.addEventListener('mouseout', function() {
+            hoverElement.style.backgroundColor = 'lightblue';
+        });
+const inputField = document.getElementById("inputField");
+
+inputField.addEventListener("keydown", function (event) {
+  console.log("Key pressed:", event.key);
+});
